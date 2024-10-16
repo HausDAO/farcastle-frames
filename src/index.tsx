8d9657c3de980f22b5682befc92b1893c483c475
earth2travis <travis.mccutcheon@gmail.com>
2024-10-16 18:15:35 -0500

    Add style view

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import { Frog } from "frog";
-import { devtools } from "frog/dev";
-import { serveStatic } from "frog/serve-static";
-import { FROG_APP_CONFIG } from "./utils/constants.js";
+import { Frog } from 'frog';
+import { devtools } from 'frog/dev';
+import { serveStatic } from 'frog/serve-static';
+import { FROG_APP_CONFIG } from './utils/constants.js';
 
-import { app as daoApp } from "./frames/dao-home.js";
-import { app as proposalApp } from "./frames/proposal.js";
-import { app as voteApp } from "./frames/vote.js";
+import { app as daoApp } from './frames/dao-home.js';
+import { app as proposalApp } from './frames/proposal.js';
+import { app as voteApp } from './frames/vote.js';
+import { app as styleApp } from './frames/style.js';
 
 export const app = new Frog(FROG_APP_CONFIG);
 
-app.frame("/", (c) => {
+// 0x2105/0x1503bd5f6f082f7fbd36438cc416cd67849c0bec
+
+app.frame('/', c => {
   return c.res({
     image:
-      "https://daohaus.mypinata.cloud/ipfs/QmaMUgNbwFpVp7KDLPPUQtuJfZ9GaySNBd6D74bizLLhsu",
+      'https://daohaus.mypinata.cloud/ipfs/QmaMUgNbwFpVp7KDLPPUQtuJfZ9GaySNBd6D74bizLLhsu',
     intents: [
       // <Button.Link href="https://warpcast.com/~/channel/farcastle/join">
       //   Join Channel
@@ -21,17 +24,18 @@ app.frame("/", (c) => {
   });
 });
 
-app.route("/dao", daoApp);
-app.route("/proposal", proposalApp);
-app.route("/proposal/vote", voteApp);
+app.route('/dao', daoApp);
+app.route('/style', styleApp);
+app.route('/proposal', proposalApp);
+app.route('/proposal/vote', voteApp);
 
-const isCloudflareWorker = typeof caches !== "undefined";
+const isCloudflareWorker = typeof caches !== 'undefined';
 if (isCloudflareWorker) {
   // @ts-ignore
-  const manifest = await import("__STATIC_CONTENT_MANIFEST");
-  const serveStaticOptions = { manifest, root: "./" };
-  app.use("/*", serveStatic(serveStaticOptions));
-  devtools(app, { assetsPath: "/", serveStatic, serveStaticOptions });
+  const manifest = await import('__STATIC_CONTENT_MANIFEST');
+  const serveStaticOptions = { manifest, root: './' };
+  app.use('/*', serveStatic(serveStaticOptions));
+  devtools(app, { assetsPath: '/', serveStatic, serveStaticOptions });
 } else {
   devtools(app, { serveStatic });
 }
