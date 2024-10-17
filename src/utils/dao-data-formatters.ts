e9a75f57abc42b322f4e9f5f65637eec54ac085f
skuhlmann <samkuhlmann@gmail.com>
2024-10-17 15:09:05 -0600

    more formatters

--- src/utils/dao-data-formatters.ts
+++ src/utils/dao-data-formatters.ts
@@ -1,5 +1,21 @@
-import { normalizeCharacters } from "./helpers";
+export const normalizeCharacters = (string: string): string => {
+  let r = string;
 
+  r = r.replace(new RegExp(/\Ξ/g), "E");
+  // r = r.replace(new RegExp(/\s/g), "");
+  // r = r.replace(new RegExp(/[àáâãäå]/g), "a");
+  // r = r.replace(new RegExp(/æ/g), "ae");
+  // r = r.replace(new RegExp(/ç/g), "c");
+  // r = r.replace(new RegExp(/[èéêë]/g), "e");
+  // r = r.replace(new RegExp(/[ìíîï]/g), "i");
+  // r = r.replace(new RegExp(/ñ/g), "n");
+  // r = r.replace(new RegExp(/[òóôõö]/g), "o");
+  // r = r.replace(new RegExp(/œ/g), "oe");
+  // r = r.replace(new RegExp(/[ùúûü]/g), "u");
+  // r = r.replace(new RegExp(/[ýÿ]/g), "y");
+  // r = r.replace(new RegExp(/\W/g), "");
+  return r;
+};
 export const truncateString = (string: string, length: number) =>
   `${string.slice(0, length)} ...`;
 
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -28,8 +28,6 @@ export const parseContent = (content: string | undefined) => {
 
 export const truncateAddress = (addr: string) =>
   `${addr.slice(0, 6)}...${addr.slice(-4)}`;
-export const truncateString = (string: string, length: number) =>
-  `${string.slice(0, length)}`;
 export const charLimit = (str: string | undefined, limit: number) =>
   str && str.length > limit ? `${str.slice(0, limit)}...` : str;
 export const fromWei = (amt: string): string => {
@@ -49,22 +47,4 @@ export const isJSON = (obj: unknown) => {
   } catch (e) {
     return false;
   }
-};
-export const normalizeCharacters = (string: string): string => {
-  let r = string;
-
-  r = r.replace(new RegExp(/\Ξ/g), "E");
-  // r = r.replace(new RegExp(/\s/g), "");
-  // r = r.replace(new RegExp(/[àáâãäå]/g), "a");
-  // r = r.replace(new RegExp(/æ/g), "ae");
-  // r = r.replace(new RegExp(/ç/g), "c");
-  // r = r.replace(new RegExp(/[èéêë]/g), "e");
-  // r = r.replace(new RegExp(/[ìíîï]/g), "i");
-  // r = r.replace(new RegExp(/ñ/g), "n");
-  // r = r.replace(new RegExp(/[òóôõö]/g), "o");
-  // r = r.replace(new RegExp(/œ/g), "oe");
-  // r = r.replace(new RegExp(/[ùúûü]/g), "u");
-  // r = r.replace(new RegExp(/[ýÿ]/g), "y");
-  // r = r.replace(new RegExp(/\W/g), "");
-  return r;
 };
